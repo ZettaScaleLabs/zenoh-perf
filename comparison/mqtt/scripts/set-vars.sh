bd594f6c98366d7afce801f074d8f292cb1740dd
Luca Cominardi <luca.cominardi@gmail.com>
2023-07-04 17:19:13 +0200

    Basic throughput tests

--- comparison/mqtt/scripts/set-vars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/false
-
-src_dir="${1:-$(pwd)}"
-lib_dir="$(realpath $src_dir/mqtt-lib/mosquitto-install)"
-export CPATH="$lib_dir/include:$CPATH"
-export LIBRARY_PATH="$lib_dir/lib:$LIBRARY_PATH"
-export LD_LIBRARY_PATH="$lib_dir/lib:$LD_LIBRARY_PATH"

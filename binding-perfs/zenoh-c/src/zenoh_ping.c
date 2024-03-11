6f010bee72d70ee51055230553c149591409eeea
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-03-11 13:38:47 +0100

    Add zenoh-c examples

---
+++ binding-perfs/zenoh-c/src/zenoh_ping.c
@@ -0,0 +1,133 @@
+#include <errno.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "zenoh.h"
+
+#define DEFAULT_PKT_SIZE 8
+#define DEFAULT_PING_NB 100
+#define DEFAULT_WARMUP_MS 1000
+#define PING_TIMEOUT_SEC 1
+
+#define handle_error_en(en, msg) \
+    do { errno = en; perror(msg); exit(EXIT_FAILURE); } while (0)
+
+z_condvar_t cond;
+z_mutex_t mutex;
+
+void callback(const z_sample_t* sample, void* context) { z_condvar_signal(&cond); }
+void drop(void* context) { z_condvar_free(&cond); }
+
+struct args_t {
+    unsigned int size;             // -s
+    unsigned int number_of_pings;  // -n
+    unsigned int warmup_ms;        // -w
+    char* config_path;             // -c
+    uint8_t help_requested;        // -h
+};
+struct args_t parse_args(int argc, char** argv);
+
+int main(int argc, char** argv) {
+    struct args_t args = parse_args(argc, argv);
+    if (args.help_requested) {
+        printf(
+            "\
+		-n (optional, int, default=%d): the number of pings to be attempted\n\
+		-s (optional, int, default=%d): the size of the payload embedded in the ping and repeated by the pong\n\
+		-w (optional, int, default=%d): the warmup time in ms during which pings will be emitted but not measured\n\
+		-c (optional, string): the path to a configuration file for the session. If this option isn't passed, the default configuration will be used.\n\
+		",
+            DEFAULT_PKT_SIZE, DEFAULT_PING_NB, DEFAULT_WARMUP_MS);
+        return 1;
+    }
+    z_mutex_init(&mutex);
+    z_condvar_init(&cond);
+    z_owned_config_t config = args.config_path ? zc_config_from_file(args.config_path) : z_config_default();
+    z_owned_session_t session = z_open(z_move(config));
+    z_keyexpr_t ping = z_keyexpr_unchecked("test/ping");
+    z_keyexpr_t pong = z_keyexpr_unchecked("test/pong");
+    z_owned_publisher_t pub = z_declare_publisher(z_loan(session), ping, NULL);
+    z_owned_closure_sample_t respond = z_closure(callback, drop, (void*)(&pub));
+    z_owned_subscriber_t sub = z_declare_subscriber(z_loan(session), pong, z_move(respond), NULL);
+    uint8_t* data = z_malloc(args.size);
+    for (int i = 0; i < args.size; i++) {
+        data[i] = i % 10;
+    }
+    z_mutex_lock(&mutex);
+    if (args.warmup_ms) {
+        printf("Warming up for %dms...\n", args.warmup_ms);
+        z_clock_t warmup_start = z_clock_now();
+
+        unsigned long elapsed_us = 0;
+        while (elapsed_us < args.warmup_ms * 1000) {
+            z_publisher_put(z_loan(pub), data, args.size, NULL);
+            int s = z_condvar_wait(&cond, &mutex);
+            if (s != 0) {
+                handle_error_en(s, "z_condvar_wait");
+            }
+            elapsed_us = z_clock_elapsed_us(&warmup_start);
+        }
+    }
+    unsigned long* results = z_malloc(sizeof(unsigned long) * args.number_of_pings);
+    for (int i = 0; i < args.number_of_pings; i++) {
+        z_clock_t measure_start = z_clock_now();
+        z_publisher_put(z_loan(pub), data, args.size, NULL);
+        int s = z_condvar_wait(&cond, &mutex);
+        if (s != 0) {
+            handle_error_en(s, "z_condvar_wait");
+        }
+        results[i] = z_clock_elapsed_us(&measure_start);
+    }
+    for (int i = 0; i < args.number_of_pings; i++) {
+        printf("%d bytes: seq=%d rtt=%luµs, lat=%luµs\n", args.size, i, results[i], results[i] / 2);
+    }
+    z_mutex_unlock(&mutex);
+    z_free(results);
+    z_free(data);
+    z_drop(z_move(sub));
+    z_drop(z_move(pub));
+    z_close(z_move(session));
+}
+
+char* getopt(int argc, char** argv, char option) {
+    for (int i = 0; i < argc; i++) {
+        size_t len = strlen(argv[i]);
+        if (len >= 2 && argv[i][0] == '-' && argv[i][1] == option) {
+            if (len > 2 && argv[i][2] == '=') {
+                return argv[i] + 3;
+            } else if (i + 1 < argc) {
+                return argv[i + 1];
+            }
+        }
+    }
+    return NULL;
+}
+
+struct args_t parse_args(int argc, char** argv) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            return (struct args_t){.help_requested = 1};
+        }
+    }
+    char* arg = getopt(argc, argv, 's');
+    unsigned int size = DEFAULT_PKT_SIZE;
+    if (arg) {
+        size = atoi(arg);
+    }
+    arg = getopt(argc, argv, 'n');
+    unsigned int number_of_pings = DEFAULT_PING_NB;
+    if (arg) {
+        number_of_pings = atoi(arg);
+    }
+    arg = getopt(argc, argv, 'w');
+    unsigned int warmup_ms = DEFAULT_WARMUP_MS;
+    if (arg) {
+        warmup_ms = atoi(arg);
+    }
+    return (struct args_t){.help_requested = 0,
+                           .size = size,
+                           .number_of_pings = number_of_pings,
+                           .warmup_ms = warmup_ms,
+                           .config_path = getopt(argc, argv, 'c')};
+}
---
+++ binding-perfs/zenoh-c/src/zenoh_sub_thr.c
@@ -0,0 +1,99 @@
+//
+// Copyright (c) 2022 ZettaScale Technology
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License 2.0 which is available at
+// http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+//
+// Contributors:
+//   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
+//
+#include <stdio.h>
+
+#include "zenoh.h"
+
+#define N 1000000
+
+typedef struct {
+    volatile unsigned long count;
+    volatile unsigned long finished_rounds;
+    z_clock_t start;
+    z_clock_t first_start;
+    bool started;
+} z_stats_t;
+
+z_stats_t *z_stats_make() {
+    z_stats_t *stats = z_malloc(sizeof(z_stats_t));
+    stats->count = 0;
+    stats->finished_rounds = 0;
+    stats->started = false;
+    return stats;
+}
+
+void on_sample(const z_sample_t *sample, void *context) {
+    z_stats_t *stats = (z_stats_t *)context;
+    if (stats->count == 0) {
+        stats->start = z_clock_now();
+        if (!stats->started) {
+            stats->first_start = stats->start;
+            stats->started = true;
+        }
+        stats->count++;
+    } else if (stats->count < N) {
+        stats->count++;
+    } else {
+        stats->finished_rounds++;
+        printf("%f msg/s\n", 1000.0 * N / z_clock_elapsed_ms(&stats->start));
+        stats->count = 0;
+    }
+}
+void drop_stats(void *context) {
+    const z_stats_t *stats = (z_stats_t *)context;
+    const unsigned long sent_messages = N * stats->finished_rounds + stats->count;
+    double elapsed_s = z_clock_elapsed_s(&stats->first_start);
+    printf("Stats being dropped after unsubscribing: sent %ld messages over %f seconds (%f msg/s)\n", sent_messages,
+           elapsed_s, (double)sent_messages / elapsed_s);
+    z_free(context);
+}
+
+int main(int argc, char **argv) {
+    z_owned_config_t config = z_config_default();
+    if (argc > 1) {
+        if (zc_config_insert_json(z_loan(config), Z_CONFIG_CONNECT_KEY, argv[1]) < 0) {
+            printf(
+                "Couldn't insert value `%s` in configuration at `%s`. This is likely because `%s` expects a "
+                "JSON-serialized list of strings\n",
+                argv[1], Z_CONFIG_CONNECT_KEY, Z_CONFIG_CONNECT_KEY);
+            exit(-1);
+        }
+    }
+
+    z_owned_session_t s = z_open(z_move(config));
+    if (!z_check(s)) {
+        printf("Unable to open session!\n");
+        exit(-1);
+    }
+
+    z_owned_keyexpr_t ke = z_declare_keyexpr(z_loan(s), z_keyexpr("test/thr"));
+
+    z_stats_t *context = z_stats_make();
+    z_owned_closure_sample_t callback = z_closure(on_sample, drop_stats, context);
+    z_owned_subscriber_t sub = z_declare_subscriber(z_loan(s), z_loan(ke), z_move(callback), NULL);
+    if (!z_check(sub)) {
+        printf("Unable to create subscriber.\n");
+        exit(-1);
+    }
+
+    char c = 0;
+    while (c != 'q') {
+        c = fgetc(stdin);
+    }
+
+    z_undeclare_subscriber(z_move(sub));
+    z_undeclare_keyexpr(z_loan(s), z_move(ke));
+    z_close(z_move(s));
+    return 0;
+}

7d1c8d71667ac21a0f0696cdc9db694cfa85a933
yuanyuyuan <az6980522@gmail.com>
2024-03-12 14:59:12 +0800

    Update build.sh

--- bindings/build.sh
+++ bindings/build.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-cmake -B ./zenoh-c/build-main
+
+rm -rf ./zenoh-c/build-main
+rm -rf ./zenoh-cpp/build-main
+cmake ./zenoh-c -B ./zenoh-c/build-main
 cmake --build ./zenoh-c/build-main
+cmake ./zenoh-cpp -B ./zenoh-cpp/build-main
+cmake --build ./zenoh-cpp/build-main
 
 git apply ./zenoh-c-cmake.patch
 git apply ./zenoh-cpp-cmake.patch
 
-cmake -B ./zenoh-c/build-tokio
+rm -rf ./zenoh-c/build-tokio
+rm -rf ./zenoh-cpp/build-tokio
+cmake ./zenoh-c -B ./zenoh-c/build-tokio
 cmake --build ./zenoh-c/build-tokio
+cmake ./zenoh-cpp -B ./zenoh-cpp/build-tokio
+cmake --build ./zenoh-cpp/build-tokio

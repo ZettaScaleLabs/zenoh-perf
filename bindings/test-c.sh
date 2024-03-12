ff95d8d357158e664b724038baa74ba1cb0b7b8e
yuanyuyuan <az6980522@gmail.com>
2024-03-12 15:16:56 +0800

    Add testing scripts

---
+++ bindings/test-c.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+LOG_DIR="_logs/zenoh-c"
+TIMEOUT=10s
+rm -rf $LOG_DIR &> /dev/null
+mkdir -p $LOG_DIR
+
+for branch in "main" "tokio"; do
+    LOG_FILE=${LOG_DIR}/${branch}.log
+    rm $LOG_FILE &> /dev/null
+    parallel --halt now,success=1 --lb <<EOL
+taskset -c 0,2 ./zenoh-c/build-${branch}/zenoh_pong -c ./config/peer-listen.json5
+sleep 1 && taskset -c 1,3 ./zenoh-c/build-${branch}/zenoh_ping -c ./config/peer-connect.json5 -s 64 -i 0 > $LOG_FILE
+sleep 1 && sleep $TIMEOUT
+EOL
+done

3d0e285bc95c4065856e8efe19db6fd191a99fa2
Oussama Teffahi <oussama.teffahi@zettascale.tech>
2024-03-12 14:21:52 +0100

    Update zenoh-cpp latency pong

--- bindings/zenoh-cpp/src/zenoh_pong.cxx
+++ bindings/zenoh-cpp/src/zenoh_pong.cxx
@@ -12,22 +12,73 @@
 //   ZettaScale Zenoh Team, <zenoh@zettascale.tech>
 //
 
+#include <chrono>
 #include <cstdio>
 #include <iostream>
+#include <thread>
 
 #include "zenoh.hxx"
 using namespace zenoh;
 
-int _main(int, char **) {
-    Config config;
+struct args_t {
+    unsigned char help_requested;        // -h
+    char* config_path;                   // -c
+};
+struct args_t parse_args(int argc, char** argv);
 
+int _main(int argc, char** argv) {
+    auto args = parse_args(argc, argv);
+
+    if (args.help_requested) {
+        std::cout << "\
+        -c (optional, string, disabled when backed by pico): the path to a configuration file for the session. If this option isn't passed, the default configuration will be used.\n\
+		";
+        return 1;
+    }
+    Config config;
+#ifdef ZENOHCXX_ZENOHC
+    if (args.config_path) {
+        config = expect<Config>(config_from_file(args.config_path));
+    }
+#endif
     auto session = expect<Session>(open(std::move(config)));
 
     auto pub = expect<Publisher>(session.declare_publisher("test/pong"));
     auto sub = expect<Subscriber>(session.declare_subscriber(
         "test/ping", [pub = std::move(pub)](const Sample &sample) mutable { pub.put(sample.get_payload()); }));
-    std::getchar();
+
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds((unsigned long)(1000)));
+    }
     return 0;
+}
+
+char* getopt(int argc, char** argv, char option) {
+    for (int i = 0; i < argc; i++) {
+        size_t len = strlen(argv[i]);
+        if (len >= 2 && argv[i][0] == '-' && argv[i][1] == option) {
+            if (len > 2 && argv[i][2] == '=') {
+                return argv[i] + 3;
+            } else if (i + 1 < argc) {
+                return argv[i + 1];
+            }
+        }
+    }
+    return NULL;
+}
+
+struct args_t parse_args(int argc, char** argv) {
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0) {
+            struct args_t args;
+            args.help_requested = 1;
+            return args;
+        }
+    }
+    struct args_t args;
+    args.help_requested = 0;
+    args.config_path = getopt(argc, argv, 'c');
+    return args;
 }
 
 int main(int argc, char **argv) {

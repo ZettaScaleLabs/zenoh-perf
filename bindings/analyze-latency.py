834bfe3b63a65ccca81b9ff879357652b002ccd8
yuanyuyuan <az6980522@gmail.com>
2024-03-14 17:32:48 +0800

    Improve analysis scripts

---
+++ bindings/analyze-latency.py
@@ -0,0 +1,40 @@
+import sys
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+
+
+assert len(sys.argv) == 2
+data_dir = Path(sys.argv[1])
+
+def percentile(p):
+    assert 0 <= p <= 100
+    return lambda vec: np.percentile(vec, p)
+
+
+MAP = {
+    'p01': percentile(1),
+    'p05': percentile(5),
+    'p50': percentile(50),
+    'p95': percentile(95),
+    'p99': percentile(99),
+}
+
+def load(file):
+    cols = str(file).split('/')
+    data = pd.read_csv(file)
+    data.columns = ['interval', 'us']
+    data = data.drop(columns=['interval'])
+    data['binding'] = cols[-2].split('-')[-1]
+    data['branch'] = cols[-3]
+    return data
+
+pd.set_option("display.precision", 2)
+
+data = pd.concat(map(load, data_dir.glob('**/latency.log')))
+data = data.groupby(['binding', 'branch']).agg(list(MAP.values()))
+data.columns = MAP.keys()
+data = data.unstack()
+
+print(data)
